89ca72833b659369b0da9ce7e55851cdad96db74
Adam Stern <stern.adam1@gmail.com>
2016-01-21 10:58:36 +1100

    Flattening export

--- index.js
+++ index.js
@@ -4,40 +4,41 @@ var Promise = require('bluebird');
 var _ = require('lodash');
 var fs = require('fs');
 
-function getTables (knex, schema) {
-  return knex
-      .select('table_schema', 'table_name')
-      .from('information_schema.tables')
-      .where('table_schema', schema)
-      .orderBy('table_name');
+function getTables(knex, schema) {
+	return knex
+		.select('table_schema', 'table_name')
+		.from('information_schema.tables')
+		.where('table_schema', schema)
+		.orderBy('table_name');
 }
 
 exports.toJSON = function (connection, schema) {
-  var knex = require('knex')({ client: 'pg', connection: connection });
+	var knex = require('knex')({client: 'pg', connection: connection});
 
-  return getTables(knex, schema)
-      .map(function (table) {
-        return knex.select('*').from(table.table_name)
-            .then(function (rows) {
+	return getTables(knex, schema)
+		.map(function (table) {
+			return knex.select('*').from(table.table_name)
+				.then(function (rows) {
 
-              var name = table.table_name;
-              name = name.substring(0, name.length - 1);
+					var name = table.table_name;
+					name = name.substring(0, name.length - 1);
 
-              return {
-                model: name,
-                data: _.map(rows, function (row) {
-                  return _.mapValues(row, function (value, key) {
-                    if (_.isArray(value)) {
-                      return "'" + new Buffer(value).toString('hex') + "'::bytea";
-                    }
+					return _.map(rows, function(row){
+						return {
+							model: name,
+							data: _.mapValues(row, function (value, key) {
+								if (_.isArray(value)) {
+									return "'" + new Buffer(value).toString('hex') + "'::bytea";
+								}
 
-                    return value;
-                  });
-                })
-              };
-            });
-      })
-      .then(function (tables) {
-        return tables;
-      });
+								return value;
+							})
+						}
+					});
+
+				});
+		})
+		.then(function (tables) {
+			return _.flatten(tables);
+		});
 };
