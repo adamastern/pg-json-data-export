c1c5848ad4ee3352b1c84eac597a09704308f222
Adam Stern <stern.adam1@gmail.com>
2016-01-21 10:41:06 +1100

    Changing export formagt

--- index.js
+++ index.js
@@ -6,34 +6,38 @@ var fs = require('fs');
 
 function getTables (knex, schema) {
   return knex
-    .select('table_schema', 'table_name')
-    .from('information_schema.tables')
-    .where('table_schema', schema)
-    .orderBy('table_name');
+      .select('table_schema', 'table_name')
+      .from('information_schema.tables')
+      .where('table_schema', schema)
+      .orderBy('table_name');
 }
 
 exports.toJSON = function (connection, schema) {
   var knex = require('knex')({ client: 'pg', connection: connection });
 
   return getTables(knex, schema)
-    .map(function (table) {
-      return knex.select('*').from(table.table_name)
-        .then(function (rows) {
-          return {
-            table: table.table_name,
-            rows: _.map(rows, function (row) {
-              return _.mapValues(row, function (value, key) {
-                if (_.isArray(value)) {
-                  return "'" + new Buffer(value).toString('hex') + "'::bytea";
-                }
+      .map(function (table) {
+        return knex.select('*').from(table.table_name)
+            .then(function (rows) {
 
-                return value;
-              });
-            })
-          };
-        });
-    })
-    .then(function (tables) {
-      return _.indexBy(tables, 'table');
-    });
+              var name = table.table_name;
+              name = name.substring(0, name.length - 1);
+
+              return {
+                model: name,
+                data: _.map(rows, function (row) {
+                  return _.mapValues(row, function (value, key) {
+                    if (_.isArray(value)) {
+                      return "'" + new Buffer(value).toString('hex') + "'::bytea";
+                    }
+
+                    return value;
+                  });
+                })
+              };
+            });
+      })
+      .then(function (tables) {
+        return tables;
+      });
 };
